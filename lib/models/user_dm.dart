7d664541e6c40d098b03059a4ced4ddc2299cff1
aymanhegazy-admin <aymanohegazy@gmail.com>
2025-05-09 08:36:05 +0300

    commit

--- lib/models/user_dm.dart
+++ lib/models/user_dm.dart
@@ -1,4 +1,9 @@
 class UserDm {
+  static UserDm? currentUser;
 
-
+  String name;
+  String id;
+  String email;
+  List<dynamic> favourite = [];
+  UserDm({required this.name, required this.id,required this.email});
 }
